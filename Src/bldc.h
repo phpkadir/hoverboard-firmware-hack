b7c4bb45b083342e37f16f8091d9d0b7889750d9
LayerDE <the_gamer@gmx.net>
2018-07-11 00:52:32 +0200

    rebuild the code of niklas #2 NOT BUILDABLE

---
+++ Src/bldc.h
@@ -0,0 +1,13 @@
+#pragma once
+
+extern int posl;
+extern int posr;
+extern unsigned int freql;
+extern unsigned int freqr;
+extern int pwml;
+extern int pwmr;
+extern int weakl;
+extern int weakr;
+extern int vel;
+
+extern float batteryVoltage;
