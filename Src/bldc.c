cc2478fe89f8fa3a2f8b8d2b6adb7b4b821da661
LayerDE <the_gamer@gmx.net>
2018-10-29 02:01:07 +0100

    Update Src/bldc.c
    updating bldc code...

--- Src/bldc.c
+++ Src/bldc.c
@@ -5,10 +5,6 @@
 #include "config.h"
 
 
-volatile int posl = 0;
-volatile int posr = 0;
-volatile unsigned int freql;  // for later inplementation
-volatile unsigned int freqr;  // for later inplementation
 volatile int pwml = 0;
 volatile int pwmr = 0;
 volatile int weakl = 0;
@@ -19,8 +15,6 @@ int lst_isr_posl = 0;
 int lst_posr = 0;
 int lst_isr_posr = 0;
 int isr_counter = 0;
-
-extern volatile int speed;
 
 extern volatile adc_buf_t adc_buffer;
 
@@ -33,7 +27,7 @@ uint8_t enable = 0;
 
 const int pwm_res = 64000000 / 2 / PWM_FREQ; // = 2000
 
-int calcWeakening(int pwm,int freq){
+int calcWeakening(int pwm,int freq){ // never used
   if (freq < START_FREQ) return 0;
   if (freq >= END_FREQ)
     return pwm*FEALD_WEAKENING_MAX/100;
@@ -150,9 +144,6 @@ inline void blockPWM(int pwm, int pos, int *u, int *v, int *w) {
   }
 }*/
 
-uint16_t buzzerTimer        = 0;
-
-int offsetcount = 0;
 int offsetrl1   = 2000;
 int offsetrl2   = 2000;
 int offsetrr1   = 2000;
@@ -162,103 +153,115 @@ int offsetdcr   = 2000;
 
 float batteryVoltage = 40.0;
 
+const int max_time = PWM_FREQ / 10; // never used
 
-// int errorl = 0;
-// int kp = 5;
-// volatile int cmdl = 0;
+typedef void (*setMotorType)(int *hPhase);
+void set_motor_r(int *hPhase){
+  RIGHT_TIM->RIGHT_TIM_U = CLAMP(hPhase[0] + pwm_res / 2, 10, pwm_res-10);
+  RIGHT_TIM->RIGHT_TIM_V = CLAMP(hPhase[1] + pwm_res / 2, 10, pwm_res-10);
+  RIGHT_TIM->RIGHT_TIM_W = CLAMP(hPhase[2] + pwm_res / 2, 10, pwm_res-10);
+}
+void set_motor_l(int *hPhase){
+  LEFT_TIM->LEFT_TIM_U = CLAMP(phase[0] + pwm_res / 2, 10, pwm_res-10);
+  LEFT_TIM->LEFT_TIM_V = CLAMP(phase[1] + pwm_res / 2, 10, pwm_res-10);
+  LEFT_TIM->LEFT_TIM_W = CLAMP(phase[2] + pwm_res / 2, 10, pwm_res-10);
+}
+setMotorType set_motor[2] = { //array for loop
+  set_motor_l,
+  set_motor_r
+};
+uint8_t get_pos_l(){
+  return hall_to_pos[!(LEFT_HALL_W_PORT->IDR & LEFT_HALL_W_PIN)]
+    [!(LEFT_HALL_V_PORT->IDR & LEFT_HALL_V_PIN)]
+      [!(LEFT_HALL_U_PORT->IDR & LEFT_HALL_U_PIN)];
+}
+uint8_t get_pos_r(){
+  return hall_to_pos[!(RIGHT_HALL_W_PORT->IDR & RIGHT_HALL_W_PIN)]
+    [!(RIGHT_HALL_V_PORT->IDR & RIGHT_HALL_V_PIN)]
+      [!(RIGHT_HALL_U_PORT->IDR & RIGHT_HALL_U_PIN)];
+}
+typedef uint8_t (*getPosType)();
+getPosType get_pos[2]={
+  get_pos_l,
+  get_pos_r
+};
 
-const int max_time = PWM_FREQ / 10;
-volatile int vel = 0;
+void calibration_func();
 
-//scan 8 channels with 2ADCs @ 20 clk cycles per sample
-//meaning ~80 ADC clock cycles @ 8MHz until new DMA interrupt =~ 100KHz
-//=640 cpu cycles
-void DMA1_Channel1_IRQHandler() {
-  isr_counter++; // I hope its a timer interrupt :)
-  DMA1->IFCR = DMA_IFCR_CTCIF1;
-  // HAL_GPIO_WritePin(LED_PORT, LED_PIN, 1);
+void nullFunc(){}
+void oldBuzzer(){
+  if (buzzerFreq != 0 && (mainCounter / 5000) % (buzzerPattern + 1) == 0) {
+    if (mainCounter % buzzerFreq == 0)
+      HAL_GPIO_TogglePin(BUZZER_PORT, BUZZER_PIN);
+  } else {
+      HAL_GPIO_WritePin(BUZZER_PORT, BUZZER_PIN, 0);
+  }
+}
+void brushless_countrol(){
+  if((currentlr[0] = ABS(adc_buffer.dcl - offsetdcl) * MOTOR_AMP_CONV_DC_AMP) > DC_CUR_LIMIT || timeout > TIMEOUT)
+    LEFT_TIM->BDTR &= ~TIM_BDTR_MOE;
+  else
+    LEFT_TIM->BDTR |= TIM_BDTR_MOE;
 
-  if(offsetcount < 1000) {  // calibrate ADC offsets
-    offsetcount++;
+  if((currentlr[1] = ABS(adc_buffer.dcr - offsetdcr) * MOTOR_AMP_CONV_DC_AMP) > DC_CUR_LIMIT || timeout > TIMEOUT)
+    RIGHT_TIM->BDTR &= ~TIM_BDTR_MOE;
+  else
+    RIGHT_TIM->BDTR |= TIM_BDTR_MOE;
+  //PWM part
+  int phase[3];
+  int wphase[3];
+  //update PWM channels based on position
+  for(int x = 0; x < 3; x++){
+    poslr[x] = get_pos[x];
+    blockPWM(pwmlr[x], poslr[x], &phase[0], &phase[1], &phase[2]);
+    if (pwmlr[x] > 0)
+      blockPWM(weaklr[x], (poslr[x]+5) % 6, &wphase[0], &wphase[1], &wphase[2]);
+    else
+      blockPWM(-weaklr[x], (poslr[x]+1) % 6, &wphase[0], &wphase[1], &wphase[2]);
+    for(int y = 0; y < 3; y++)
+      phase[y] += wphase[y];
+    set_motor[x](phase);
+    //speed measurung
+    timer[x]++;
+    if(last_pos[x]!=poslr[x]){
+      phase_period[x] = timer[x];
+      timer[x] = 0;
+    } else if(timer[x] > phase_period[x])
+      timer[x] = phase_period[x];
+  }
+}
+
+typedef void (*IsrPtr)();
+
+volatile IsrPtr timer_brushless = calibration_func;
+volatile IsrPtr buzzerFunc = nullFunc;
+void calibration_func(){
+  if(mainCounter < 1024) {  // calibrate ADC offsets
     offsetrl1 = (adc_buffer.rl1 + offsetrl1) / 2;
     offsetrl2 = (adc_buffer.rl2 + offsetrl2) / 2;
     offsetrr1 = (adc_buffer.rr1 + offsetrr1) / 2;
     offsetrr2 = (adc_buffer.rr2 + offsetrr2) / 2;
     offsetdcl = (adc_buffer.dcl + offsetdcl) / 2;
     offsetdcr = (adc_buffer.dcr + offsetdcr) / 2;
-    return;
   }
-
-  if (buzzerTimer % 100 == 0) {
-    batteryVoltage = batteryVoltage * 0.999 + ((float)adc_buffer.batt1 * ADC_BATTERY_VOLT) * 0.001;
+  else{
+    timer_brushless = brushless_countrol;
   }
+}
 
-
-  #ifdef BEEPS_BACKWARD
-    if (speed < -50 && enable == 1) {
-      buzzerFreq = 5;
-      buzzerPattern = 1;
-    } else if (enable == 1) {
-      buzzerFreq = 0;
-      buzzerPattern = 1;
-    }
-  #endif
-
-
-  //disable PWM when current limit is reached (current chopping)
-  if(ABS((adc_buffer.dcl - offsetdcl) * MOTOR_AMP_CONV_DC_AMP) > DC_CUR_LIMIT || timeout > TIMEOUT || enable == 0)
-    LEFT_TIM->BDTR &= ~TIM_BDTR_MOE;
-  else
-    LEFT_TIM->BDTR |= TIM_BDTR_MOE;
-
-  if(ABS((adc_buffer.dcr - offsetdcr) * MOTOR_AMP_CONV_DC_AMP)  > DC_CUR_LIMIT || timeout > TIMEOUT || enable == 0)
-    RIGHT_TIM->BDTR &= ~TIM_BDTR_MOE;
-  else
-    RIGHT_TIM->BDTR |= TIM_BDTR_MOE;
-
-  //determine next position based on hall sensors
-  posl = hall2pos[!(LEFT_HALL_W_PORT->IDR & LEFT_HALL_W_PIN)][!(LEFT_HALL_V_PORT->IDR & LEFT_HALL_V_PIN)][!(LEFT_HALL_U_PORT->IDR & LEFT_HALL_U_PIN)];
-  posr = hall2pos[!(RIGHT_HALL_W_PORT->IDR & RIGHT_HALL_W_PIN)][!(RIGHT_HALL_V_PORT->IDR & RIGHT_HALL_V_PIN)][!(RIGHT_HALL_U_PORT->IDR & RIGHT_HALL_U_PIN)];
-
-  if (posl != lst_posl) {
-    freql = isr_counter - lst_isr_posl;
-    lst_isr_posl = isr_counter;
-  }
-  else if (freql < isr_counter - lst_isr_posl)
-    freql = isr_counter - lst_isr_posl;
-  if (posr != lst_posr) {
-    freqr = isr_counter - lst_isr_posr;
-    lst_isr_posr = isr_counter;
-  }
-  else if (freqr < isr_counter - lst_isr_posr)
-    freqr = isr_counter - lst_isr_posr;
-  //blockPhaseCurrent(posl, adc_buffer.rl1 - offsetrl1, adc_buffer.rl2 - offsetrl2, &curl);
-
-  //setScopeChannel(2, (adc_buffer.rl1 - offsetrl1) / 8);
-  //setScopeChannel(3, (adc_buffer.rl2 - offsetrl2) / 8);
-
-  //create square wave for buzzer
-  buzzerTimer++;
-  if (buzzerFreq != 0 && (buzzerTimer / 5000) % (buzzerPattern + 1) == 0)
-    if (buzzerTimer % buzzerFreq == 0)
-      HAL_GPIO_TogglePin(BUZZER_PORT, BUZZER_PIN);
-  else
-      HAL_GPIO_WritePin(BUZZER_PORT, BUZZER_PIN, 0);
-
-  //update PWM channels based on position
-  int ul, vl, wl, ur, vr, wr;
-  blockPWM(pwml, posl, &ul, &vl, &wl);
-  blockPWM(pwmr, posr, &ur, &vr, &wr);
-
-  int weakul, weakvl, weakwl, weakur, weakvr, weakwr;
-  blockPWM(calcWeakening(pwml, freql), (pwml > 0) ? (posl+5) % 6 : (posl+1) % 6, &weakul, &weakvl, &weakwl);
-  blockPWM(calcWeakening(pwmr, freqr), (pwmr > 0) ? (posr+5) % 6 : (posr+1) % 6, &weakur, &weakvr, &weakwr);
-
-  LEFT_TIM->LEFT_TIM_U = CLAMP(ul + weakul + pwm_res / 2, 10, pwm_res-10);
-  LEFT_TIM->LEFT_TIM_V = CLAMP(vl + weakvl + pwm_res / 2, 10, pwm_res-10);
-  LEFT_TIM->LEFT_TIM_W = CLAMP(wl + weakwl + pwm_res / 2, 10, pwm_res-10);
-
-  RIGHT_TIM->RIGHT_TIM_U = CLAMP(ur + weakur + pwm_res / 2, 10, pwm_res-10);
-  RIGHT_TIM->RIGHT_TIM_V = CLAMP(vr + weakvr + pwm_res / 2, 10, pwm_res-10);
-  RIGHT_TIM->RIGHT_TIM_W = CLAMP(wr + weakwr + pwm_res / 2, 10, pwm_res-10);
+//scan 8 channels with 2ADCs @ 20 clk cycles per sample
+//meaning ~80 ADC clock cycles @ 8MHz until new DMA interrupt =~ 100KHz
+//=640 cpu cycles
+void DMA1_Channel1_IRQHandler() {
+  DMA1->IFCR = DMA_IFCR_CTCIF1;
+  mainCounter++;
+  buzzerFunc();
+  timer_brushless();
+  //HAL_GPIO_WritePin(LED_PORT, LED_PIN, 1);
+  //HAL_GPIO_WritePin(LED_PORT, LED_PIN, 0);
+  if (mainCounter % 1000 == 0) {  // because you get float rounding errors if it would run every time
+    batteryVoltage = batteryVoltage * 0.99 + ((float)adc_buffer.batt1 * ((float)BAT_CALIB_REAL_VOLTAGE / (float)BAT_CALIB_ADC)) * 0.01;
+  }// murks
+  blockPhaseCurrent(poslr[0], adc_buffer.rl1 - offsetrl1, adc_buffer.rl2 - offsetrl2, &blockcurlr[0]); //Old shitty code
+  blockPhaseCurrent(poslr[1], adc_buffer.rr1 - offsetrr1, adc_buffer.rr2 - offsetrr2, &blockcurlr[1]); //Old shitty code
 }
