261d1a645a84bd37ad330285a3f1b22961228b72
LayerDE <the_gamer@gmx.net>
2018-11-29 22:18:00 +0100

    Added algorythm to calculate wheels

--- Inc/control.h
+++ Inc/control.h
@@ -1,5 +1,6 @@
 #pragma once
 extern uint32_t timeout;
-
 int clean_adc(int inval);
 int calc_torque(int throttle,int breaks);
+void calc_torque_per_wheel(int throttle, float steering_eagle, int* torque);
+float calc_steering_eagle(int inval);
--- Src/control.c
+++ Src/control.c
@@ -132,12 +132,13 @@ int clean_adc(int inval){
 #define WHEELBASE 2
 #define WHEEL_WIDTH 1
 #define STEERING_TO_WHEEL_DIST 1
+
 inline void calc_torque_per_wheel(int throttle, float steering_eagle, int* torque){
   int back_wheel = WHEELBASE / tan(abs(steering_eagle));
   int radius_main = sqrt(pow(back_wheel, 2)+pow(WHEELBASE / 2 ,2));
 #if !defined(STEERING)
-  torque[0] = (back_wheel + WHEEL_WIDTH/2 * sign(steering_eagle)) * throttle / radius_main;
-  torque[1] = (back_wheel - WHEEL_WIDTH/2 * sign(steering_eagle)) * throttle / radius_main;
+  torque[0] = (back_wheel + WHEEL_WIDTH/2 * SIGN(steering_eagle)) * throttle / radius_main;
+  torque[1] = (back_wheel - WHEEL_WIDTH/2 * SIGN(steering_eagle)) * throttle / radius_main;
 #else
   #define wheel_bl (back_wheel + (WHEEL_WIDTH/2 * sign(steering_eagle) - STEERING_TO_WHEEL_DIST))
   #define wheel_br (back_wheel - (WHEEL_WIDTH/2 * sign(steering_eagle) - STEERING_TO_WHEEL_DIST))
@@ -147,6 +148,7 @@ inline void calc_torque_per_wheel(int throttle, float steering_eagle, int* torqu
   #undef wheel_br
 #endif
 }
+
 int calc_torque(int throttle,int breaks){
   if(breaks == 0){  // drive forward
     return throttle;
@@ -157,5 +159,8 @@ int calc_torque(int throttle,int breaks){
   else{
     return 0;
   }
-
+}
+#define STEERING_EAGLE_FACTOR 0.01
+float calc_steering_eagle(int inval){
+  return (float)inval * STEERING_EAGLE_FACTOR;
 }
