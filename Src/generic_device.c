1a6b1b0a6c5e2cc786b1ea80c453d805bd91fd3e
LayerDE <the_gamer@gmx.net>
2020-07-28 13:36:27 +0200

    back to stock

--- Src/generic_device.c
+++ Src/generic_device.c
@@ -47,7 +47,8 @@ int clean_adc_half(uint32_t inval){
 void device_specific(){
   int tmp = -clean_adc_full(value_buffer(virtual_ival[0][0],0));
   int tmp2 = clean_adc_half(value_buffer(virtual_ival[0][1],1));
-  int tmp3 = ((tmp * tmp * SIGN(tmp) / THROTTLE_MAX ) * 2 + tmp ) * tmp2 / THROTTLE_MAX / 3;
+  int tmp3 = //((tmp * tmp * SIGN(tmp) / THROTTLE_MAX ) * 2 + tmp ) * tmp2 / THROTTLE_MAX / 3;
+  tmp * tmp2 / THROTTLE_MAX;
   if(tmp3 < 0) {
     tmp3 = tmp3 * THROTTLE_REVERSE_MAX * (-1) / THROTTLE_MAX;
     if(tmp3 < THROTTLE_REVERSE_MAX / 10)
